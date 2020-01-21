aa3196c5e607e9ff18120e045c866ba5014b8470
Simon Beckett <simon@redballoon.com.au>
2020-01-21 17:37:10 +1100

    Settings menu testing - I should really use a loc al dev

--- octoprint_OctoHue/__init__.py
+++ octoprint_OctoHue/__init__.py
@@ -1,18 +1,10 @@
 # coding=utf-8
 from __future__ import absolute_import
-
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
 
-class OctohuePlugin(octoprint.plugin.SettingsPlugin,
+class OctohuePlugin(octoprint.plugin.StartupPlugin,
+					octoprint.plugin.SettingsPlugin,
                     octoprint.plugin.AssetPlugin,
                     octoprint.plugin.TemplatePlugin,
 					octoprint.plugin.EventHandlerPlugin):
@@ -48,7 +40,7 @@ class OctohuePlugin(octoprint.plugin.SettingsPlugin,
 	
 	def get_template_configs(self):
 		return [
-			dict(type="settings", custom_bindings=True)
+			dict(type="settings", custom_bindings=False)
 		]
 
 	##~~ AssetPlugin mixin
