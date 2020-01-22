00e266b6c1c2d5a48f97535bf445f00a24cf6c5c
Simon Beckett <simon@redballoon.com.au>
2020-01-22 21:52:34 +1100

    Typo in plugin_identifier. Fixed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "octoHue"
+plugin_identifier = "octohue"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_octohue"
