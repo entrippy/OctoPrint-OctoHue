8b8acfcb20d902cef507259557a1f380cc64741e
Simon Beckett <simon@redballoon.com.au>
2020-01-22 21:39:46 +1100

    I removed something at some time

---
+++ octoprint-octohue/__init__.py
@@ -0,0 +1,101 @@
+# coding=utf-8
+from __future__ import absolute_import
+import octoprint.plugin
+
+
+class OctohuePlugin(octoprint.plugin.StartupPlugin,
+					octoprint.plugin.SettingsPlugin,
+                    octoprint.plugin.AssetPlugin,
+                    octoprint.plugin.TemplatePlugin,
+					octoprint.plugin.EventHandlerPlugin):
+
+	def on_after_startup(self):
+		self._logger.info("Octohue is alive!")
+
+	##~~ SettingsPlugin mixin
+
+	def get_settings_defaults(self):
+		return dict(
+			bridgeaddr="",
+			husername="",
+			lampID="",
+			defaultbri="",
+			onc="#FFFFFF",
+			completec="#33FF36",
+			errorc="#FF0000",
+			warningc="#FFC300"
+		)
+
+	def get_template_vars(self):
+		return dict(
+			bridgeaddr=self._settings.get(["bridgeaddr"]),
+			husername=self._settings.get(["husername"]),
+			lampID=self._settings.get(["lampID"]),
+			defaultbri=self._settings.get(["defaultbri"]),
+			onc=self._settings.get(["onc"]),
+			completec=self._settings.get(["completec"]),
+			errorc=self._settings.get(["errorc"]),
+			warningc=self._settings.get(["warningc"])
+		)
+	
+	def get_template_configs(self):
+		return [
+			dict(type="settings", custom_bindings=False)
+		]
+
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		# Define your plugin's asset files to automatically include in the
+		# core UI here.
+		return dict(
+			js=["js/OctoHue.js"],
+			css=["css/OctoHue.css"],
+			less=["less/OctoHue.less"]
+		)
+
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		# Define the configuration for your plugin to use with the Software Update
+		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+		# for details.
+		return dict(
+			OctoHue=dict(
+				displayName="Octohue Plugin",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="entrippy",
+				repo="OctoPrint-OctoHue",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/entrippy/OctoPrint-OctoHue/archive/{target_version}.zip"
+			)
+		)
+
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+__plugin_name__ = "Octohue"
+
+# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
+# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
+# compatibility flags according to what Python versions your plugin supports!
+#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
+#__plugin_pythoncompat__ = ">=3,<4" # only python 3
+#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = OctohuePlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
