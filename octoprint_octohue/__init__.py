4faded8e9e297a2425956911e1a695a1f4fdea66
Simon Beckett <simon@redballoon.com.au>
2020-02-01 15:22:01 +1100

    Updated release version, reset python compability flag

--- octoprint_octohue/__init__.py
+++ octoprint_octohue/__init__.py
@@ -151,7 +151,7 @@ __plugin_name__ = "Octohue"
 # compatibility flags according to what Python versions your plugin supports!
 #__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
 #__plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
 	global __plugin_implementation__
