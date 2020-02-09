d22e020dd9875d36499b412f922fc303d48291bb
Simon Beckett <simon@redballoon.com.au>
2020-02-09 21:46:20 +1100

    Cleaned up some comments

--- octoprint_octohue/static/js/OctoHue.js
+++ octoprint_octohue/static/js/OctoHue.js
@@ -8,10 +8,6 @@ $(function() {
     function OctohueViewModel(parameters) {
         var self = this;
 
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
-
         self.togglehue = function() {
             OctoPrint.simpleApiCommand("octohue", "togglehue", {}, {});
         };
@@ -23,10 +19,7 @@ $(function() {
      */
     OCTOPRINT_VIEWMODELS.push({
         construct: OctohueViewModel,
-        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
         dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
-        // dependencies: [ NavigationViewModel ],
-        // Elements to bind to, e.g. #settings_plugin_OctoHue, #tab_plugin_OctoHue, ...
         elements: [ "#navbar_plugin_octohue" ]
     });
 });
