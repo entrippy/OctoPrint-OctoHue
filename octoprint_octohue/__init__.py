4b04f8f5a160c0a4bac898c2bd4de9d0ee10ae08
Simon Beckett <sirsimonbeckett@gmail.com>
2023-11-07 22:09:11 +1100

    using the new all_events feature that wasn't here when I first did this

--- octoprint_octohue/__init__.py
+++ octoprint_octohue/__init__.py
@@ -188,21 +188,6 @@ class OctohuePlugin(octoprint.plugin.StartupPlugin,
 		}
 		return settings
 
-	def create_status(self, status, colour=None, brightness=None, delay=None, turnoff=False):
-		self._settings.set(
-			["statusDict", status, "colour"], colour
-		)
-		self._settings.set(
-			["statusDict", status, "brightness"], brightness
-		)
-		self._settings.set(
-			["statusDict", status, "delay"], delay
-		)
-		self._settings.set(
-			["statusDict", status, "turnoff"], turnoff
-		)
-		self._settings.save()
-
 	def get_settings_restricted_paths(self):
 		return dict(admin=[["bridgeaddr"],["husername"]])
 	
@@ -218,7 +203,10 @@ class OctohuePlugin(octoprint.plugin.StartupPlugin,
 			
 		self._settings.save()
 
-
+	def on_settings_load(self):
+		my_settings = {
+            "availableEvents": octoprint.events.all_events()
+		}
 	def on_settings_save(self, data):
 		self._logger.info("Saving: %s" % data) 
 		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
