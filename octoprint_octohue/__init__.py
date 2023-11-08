7b8d779fc4a4597af4f7d13f64e6c382e039edd4
Simon Beckett <sirsimonbeckett@gmail.com>
2023-11-08 19:52:25 +1100

    tyoe on the next statement

--- octoprint_octohue/__init__.py
+++ octoprint_octohue/__init__.py
@@ -121,7 +121,7 @@ class OctohuePlugin(octoprint.plugin.StartupPlugin,
 	# Trigger state on Status match
 	def on_event(self, event, payload):
 		self._logger.debug("Recieved Status: %s from Printer" % event)
-		with next(status for status in self._settings.get(['statusDict']) if status['status'] == event)
+		with next(status for status in self._settings.get(['statusDict']) if status['status'] == event):
 			self._logger.info("Received Configured Status Event: %s" % event)
 			delay = status['delay'] or 0
 
