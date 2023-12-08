1ce1213c09c2a4fee1f0e6b8181293931d273260
Simon Beckett <sirsimonbeckett@gmail.com>
2023-12-08 20:33:45 +1100

    bumpy the release and removing colourmath and numpy

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_octohue"
 plugin_name = "OctoHue"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.6.0"
+plugin_version = "0.7.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/entrippy/OctoPrint-OctoHue"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["colormath", "qhue", "numpy"]
+plugin_requires = ["qhue"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
