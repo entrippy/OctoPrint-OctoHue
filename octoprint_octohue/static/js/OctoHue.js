3f587c45e1151d1d222ff0cd890c0d7d6bbafe21
Simon Beckett <sirsimonbeckett@gmail.com>
2023-12-11 22:28:56 +1100

    final pairing steps

--- octoprint_octohue/static/js/OctoHue.js
+++ octoprint_octohue/static/js/OctoHue.js
@@ -121,6 +121,38 @@ $(function() {
 			});
         }
 
+        self.bridgepair = function() {
+            var bridgebutton= document.getElementById("huebridge_pairingbutton");
+		    bridgebutton.innerHTML = '<i class="fa fa-link"></i> Pairing...';
+		    bridgebutton.disabled = true;
+		    var pairing_try_count = 0;
+		    var text_pairing_count = document.getElementById("huebridge_pairingreties");
+		    text_pairing_count.innerHTML = "Try count: " + pairing_try_count + "/30";
+		    document.getElementById("huebridge_startsearch").disabled = true;
+		    var interval_pairing = setInterval(function() {
+			pairing_try_count += 1;
+			text_pairing_count.innerHTML = "Try count: " + pairing_try_count + "/30";
+            OctoPrint.simpleApiCommand("octohue", "bridge", {"pair": "bridgeaddr"}, {}).done(function(response) {
+				console.info(response.response);
+				if(response.reponse == "success")
+				{
+					clearInterval(interval_pairing);
+					text_pairing_count.innerHTML = "<font color='green'>Succesfull Pairing !</font>";
+					bridgeConfiguration();
+				}
+			})
+
+			if(pairing_try_count == 30)
+			{
+				clearInterval(interval_pairing);
+				pairing_bridge_button.innerHTML = '<i class="fa fa-link"></i> Start Pairing';
+				pairing_bridge_button.disabled = false;
+				text_pairing_count.innerHTML = "<font color='red'>Unable to pair. Please try again</font>";
+				document.getElementById("huebridge_startsearch").disabled = false;
+			}
+		}, 1000);
+        }
+
         self.onBeforeBinding = function () {
             self.settings = self.settingsViewModel.settings;
             self.ownSettings = self.settings.plugins.octohue;
