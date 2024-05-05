49321bc16fbb5c68371414002da4e2e5f3b1f8eb
Simon Beckett <sirsimonbeckett@gmail.com>
2024-05-05 11:39:59 +1000

    Adding cooldown function

--- octoprint_octohue/__init__.py
+++ octoprint_octohue/__init__.py
@@ -196,7 +196,7 @@ class OctohuePlugin(octoprint.plugin.StartupPlugin,
 
 		while True:
 			current_temp = self._printer.get_current_temperatures()['tool0']['actual']
-			self._logger.debug(f"Tool Temp: {current_temp}")
+			self._logger.debug(f"Tool Temp: {current_temp}, PowerOff Temp: {self._settings.get(['powerofftemp'])}")
 			# Check if current_temp is below shutdowntemp OR below 40 (whichever happens first)
 			if current_temp < self._settings.get(['powerofftemp']) or current_temp < 40:
 				print(f"Temperature reached {current_temp}, shutting down.")
